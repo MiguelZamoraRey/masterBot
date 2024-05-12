dbe501702f7cc8655a4aeaae9efbe506213e8969
miguelzamorarey <miguelzamorarey@gmail.com>
2024-05-12 09:24:35 +0200

    add logs to messages

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,10 @@ dotenv.config();
 
 const TOKEN = process.env.DISCORD_TOKEN;
 
-// Crea una instancia del cliente de Discord
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Define el prefijo para los comandos, en este caso, no usaremos prefijo
 const prefix = '';
 
-// Cuando el bot esté listo, imprime un mensaje en la consola
 client.once('ready', () => {
   if (TOKEN && TOKEN?.length > 0) {
     console.log('Bot listo!');
@@ -19,18 +16,12 @@ client.once('ready', () => {
   }
 });
 
-// Escucha los mensajes
 client.on('message', (message: Message) => {
-  // Ignora los mensajes del bot
   if (message.author.bot) return;
-
   console.log('Mensaje recibido:', message.content);
-  // Si el mensaje es "holi"
   if (message.content.toLowerCase() === 'holi') {
-    // Envía "holi goli" como respuesta
     message.channel.send('holi goli');
   }
 });
 
-// Autentica el bot con el token proporcionado por Discord Developer Portal
 client.login(TOKEN).catch(console.error);
